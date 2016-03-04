8039e9efb423448780212cb345534ede156831a3
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-04 07:55:00 -0800

    playing with restructuring and dicts within dicts

--- src/mailroom.py
+++ src/mailroom.py
@@ -1,7 +1,4 @@
 # -*- coding: utf-8 -*-
-prompts = {
-    "welcome": "Please enter 'thank you' to write a letter, or 'report' to create a report (or 'list' to see all options: \n",
-}
 
 
 def thank_you():
@@ -12,31 +9,53 @@ def create_report():
     print("create report selected")
 
 
-def list_options():
-    for key in main_menu:
-        print key
 
 
-def end_program():
-    print("you can't quit now!")
-
-
-main_menu = {
-    "thank you": thank_you,
-    "report": create_report,
-    "list": list_options,
-    "quit": end_program
+menus = {
+    "main": {
+        "message": "Please enter thank you or report:\n",
+        "menu_options": {
+            "thank you": thank_you,
+            "report": create_report,
+        },
+    },
 }
 
 
-def get_input(prompt, menu_options):
+def menu_info(menu):
+    return menu["message"], menu["menu_options"]
+
+
+def get_menu(menu_name):
+    return menus[menu_name]
+
+
+def end_program():
+    print("ending!")
+
+
+def check_quit(user_answer):
+    if user_answer == "quit":
+        confirm = raw_input("Are you sure? \n")
+        return confirm
+
+
+def get_input(prompt, options):
     user_answer = raw_input(prompt)
-    if user_answer not in menu_options:
-        get_input(prompt, menu_options)
+    if user_answer not in options:
+        get_input(prompt, options)
     return user_answer
 
 
-def welcome_prompt():
-    print('welcome message')
-    choice = get_input(prompts["welcome"], main_menu)
-    main_menu[choice]()
+def prompt_user(active_menu="main", hardcode_input=""):
+    choice = hardcode_input
+    message, options = menu_info(get_menu(active_menu))
+    if not hardcode_input:
+        choice = get_input(message, options)
+    if check_quit(choice) == "y":
+        end_program(active_menu)
+    options[choice]()
+    return choice
+
+
+prompt_user("main")
