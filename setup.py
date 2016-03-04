b5eeecb65d68c0e37d6bf8cdddb762da9f782ddf
SELENA FLANNERY <SELENA.FLANNERY@GMAIL.COM>
2016-03-03 16:15:34 -0800

    missing files

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+
+setup(
+    name="mailroom",
+    description="Allows user to interact with donor information for charity.",
+    version=0.1,
+    author="Selena Flannery and Richard Tesmond",
+    author_email="tesmonrd@gmail.com",
+    license="MIT",
+    py_modules=["mailroom"],
+    package_dir={"": "src"},
+    install_requires=[],
+    extras_require={'test': ['pytest', 'pytest-xdist', 'tox']},
+)
